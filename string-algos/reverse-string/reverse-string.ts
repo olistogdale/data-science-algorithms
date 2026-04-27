b2d1a4a0beb7b99d2a4a7843477acb60fb07f887
Oli Stogdale <oli.stogdale@gmail.com>
2026-04-27 17:24:19 +0100

    feat: implemented solution to the reverse string (in place) problem.

---
+++ string-algos/reverse-string/reverse-string.ts
@@ -0,0 +1,31 @@
+/*
+* Problem: Reverse String
+* Description: Reverse an input string in place.
+* Approach: Two pointers
+* Best-case Time: O(n)
+* Worst-case Time: O(n)
+* Space: O(1) 
+*/
+
+const stringArray1 = ['h','e','l','l','o'];
+const stringArray2 = ['g','o','o','d','b','y','e'];
+
+function reverseString(strArr: string[]) {
+  let start: number = 0;
+  let end: number = strArr.length - 1;
+
+  while (start < end) {
+    [strArr[start], strArr[end]] = [strArr[end], strArr[start]];
+    start++
+    end--
+  }
+}
+
+console.log(stringArray1);
+console.log(stringArray2);
+
+reverseString(stringArray1);
+reverseString(stringArray2);
+
+console.log(stringArray1);
+console.log(stringArray2);
